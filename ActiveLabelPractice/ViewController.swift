732ced0a76418ba46040d82e41d9b5c6cc034103
Masateru maegawa <masa.mg283@gmail.com>
2021-03-12 18:41:08 +0900

    Initial Commit

---
+++ ActiveLabelPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActiveLabelPractice
+//
+//  Created by Masateru Maegawa on 2021/03/12.
+//  Copyright © 2021 Masateru Maegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
